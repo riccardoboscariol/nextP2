86362c81f76e7f12d96c62d45a4b3a3b3bcace4e
riccardoboscariol <138816765+riccardoboscariol@users.noreply.github.com>
2025-03-04 15:38:42 +0100

    idea2.py

--- idea2.py
+++ idea2.py
@@ -64,15 +64,13 @@ def load_sheet_data(sheet, max_retries=3):
 
 # Frasi target e di controllo
 target_phrases = [
-    {"frase": "On March 4, 2025, in the Champions League football match Bruges vs. Aston Villa, Bruges will win the match.", "feedback": "We do not know if this statement is true or false."},
-    {"frase": "On March 4, 2025, in the Champions League football match Real Madrid vs. Atletico Madrid, Real Madrid will win the match.", "feedback": "We do not know if this statement is true or false."},
-    {"frase": "On March 4, 2025, in the Champions League football match PSV Eindhoven vs. Arsenal, PSV Eindhoven will win the match.", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On March 4, 2025, in the Champions League football match Borussia Dortmund vs. Lille, Borussia Dortmund will win the match.", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On 20 March 2025, the Apple stock will be higher than on 15 February 2025"},
 ]
 
 control_phrases = [
-    {"frase": "On February 25, 2025, in the Coppa Italia football match Bruges vs. Aston Villa, Bruges will lose the match.", "feedback": "We do not know if this statement is true or false."},
-    {"frase": "On February 25, 2025, in the Coppa Italia football match Real Madrid vs. Atletico Madrid, Real Madrid will lose the match.", "feedback": "We do not know if this statement is true or false."},
-    {"frase": "On March 4, 2025, in the Champions League football match PSV Eindhoven vs. Arsenal, PSV Eindhoven will lose the match.", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On February 25, 2025, in the Coppa Italia football match Borussia Dortmund vs. Lille, Borussia Dortmund will lose the match.", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On 20 March 2025, the Apple stock will be lower than on 15 February 2025"},
 ]
 
 # Frasi di test
