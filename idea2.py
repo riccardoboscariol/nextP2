0d1dfd5fcfcdeaebbfd0c71ea73c68067f53d515
riccardoboscariol <138816765+riccardoboscariol@users.noreply.github.com>
2025-04-30 16:03:19 +0200

    idea2.py

--- idea2.py
+++ idea2.py
@@ -60,13 +60,13 @@ def load_sheet_data(sheet, max_retries=3):
 
 # Frasi target e di controllo
 target_phrases = [
-    {"frase": "On April 29, 2025, in the Champions League football match Arsenal vs. PSG, Arsenal will win the match.", "feedback": "We do not know if this statement is true or false."},
-    {"frase": "On April 30, 2025, in the Champions League football match Barcellona vs. Inter, Barcellona will win the match.", "feedback": "We do not know if this statement is true or false."}
+    {"frase": "On May 1, 2025, in the Europa League football match Athletic Bilbao vs. Manchester United, Athletic Bilbao will win the match.", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On May 1, 2025, in the Europa League football match Tottenham vs. Bodø/Glimt, Tottenham will win the match.", "feedback": "We do not know if this statement is true or false."}
 ]
 
 control_phrases = [
-    {"frase": "On April 29, 2025, in the Champions League football match Arsenal vs. PSG, Arsenal will lose the match.", "feedback": "We do not know if this statement is true or false."},
-    {"frase": "On April 30, 2025, in the Champions League football match Barcellona vs. Inter, Barcellona will lose the match.", "feedback": "We do not know if this statement is true or false."}
+    {"frase": "On May 1, 2025, in the Europa League football match Athletic Bilbao vs. Manchester United, Athletic Bilbao will lose the match.", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On May 1, 2025, in the Europa League football match Tottenham vs. Bodø/Glimt, Tottenham will lose the match.", "feedback": "We do not know if this statement is true or false."}
 ]
 
 # Frasi di test con risposte
