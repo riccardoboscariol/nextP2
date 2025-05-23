4047104e8f2ee89351472e04f3e813284c4bc687
riccardoboscariol <138816765+riccardoboscariol@users.noreply.github.com>
2025-05-23 21:24:37 +0200

    idea2.py

--- idea2.py
+++ idea2.py
@@ -60,11 +60,11 @@ def load_sheet_data(sheet, max_retries=3):
 
 # Frasi target e di controllo
 target_phrases = [
-    {"frase": "On May 14, 2025, in the Coppa Italia football match Milan vs. Bologna, Milan will win against Bologna", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On May 24, 2025, in the Serie A football match Empoli vs. Verona, Empoli will win against Verona", "feedback": "We do not know if this statement is true or false."},
 ]
 
 control_phrases = [
-    {"frase": "On May 14, 2025, in the Coppa Italia football match Milan vs. Bologna, Bologna will win against Milan", "feedback": "We do not know if this statement is true or false."},
+    {"frase": "On May 24, 2025, in the Serie A football match Empoli vs. Verona, Verona will win against Empoli", "feedback": "We do not know if this statement is true or false."},
 
 ]
 
